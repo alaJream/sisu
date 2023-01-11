e6c58c9d684667218add9170f3a5f7f262a74b44
Rosa Regalado <rosaregalado101@gmail.com>
2023-01-10 23:36:58 -0800

    updated gitignore

--- venv-django-upgrade/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rosa/Desktop/code/ACS-second-year/ACS-term3/sisu-vr/sisu/venv-django-upgrade/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
